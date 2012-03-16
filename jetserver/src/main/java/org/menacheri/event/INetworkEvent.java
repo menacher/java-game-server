cfa4bfc7e4a771166bd1c8aa0372dbb6911562c3
menacher <abrahammenacherry@gmail.com>
2012-03-16 19:17:48 +0530

    Initial commit of NetworkEvent interface.

---
+++ jetserver/src/main/java/org/menacheri/event/INetworkEvent.java
@@ -0,0 +1,20 @@
+package org.menacheri.event;
+
+import org.menacheri.communication.IDeliveryGuaranty;
+
+/**
+ * This interface is specifically used for events that will get transmitted to
+ * remote machine/vm. It contains the {@link IDeliveryGuaranty} associated with
+ * the event so that messages can be transmitted either using TCP or UDP
+ * transports based on the guaranty defined. Implementations can use RELIABLE as
+ * default.
+ * 
+ * @author Abraham Menacherry
+ * 
+ */
+public interface INetworkEvent extends IEvent
+{
+	IDeliveryGuaranty getDeliveryGuaranty();
+
+	void setDeliveryGuaranty(IDeliveryGuaranty deliveryGuaranty);
+}
