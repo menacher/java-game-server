a3744155f613616450769af48af6c5b54947e5ea
menacher <abrahammenacherry@gmail.com>
2012-03-22 16:57:02 +0530

    Added close method. Also added dependency on session registry service in order to remove session when message sender is closed.

--- jetserver/src/main/java/org/menacheri/communication/NettyUDPMessageSender.java
+++ jetserver/src/main/java/org/menacheri/communication/NettyUDPMessageSender.java
@@ -7,27 +7,40 @@ import org.menacheri.app.ISession;
 import org.menacheri.communication.IDeliveryGuaranty.DeliveryGuaranty;
 import org.menacheri.communication.IMessageSender.IFast;
 import org.menacheri.event.Events;
-
+import org.menacheri.event.impl.NetworkEvent;
+import org.menacheri.handlers.netty.UDPUpstreamHandler;
+import org.menacheri.service.ISessionRegistryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is used to send messages to a remote UDP client or server. An
- * instance of this class will be created when the {@link Events#CONNECT_UDP}
- * event is sent to a {@link ISession}
+ * instance of this class will be created by the {@link UDPUpstreamHandler} when
+ * a {@link Events#CONNECT} event is received from client. The created instance
+ * of this class is then sent as payload of a {@link NetworkEvent} to the
+ * {@link ISession}.
+ * 
  * 
  * @author Abraham Menacherry
  * 
  */
 public class NettyUDPMessageSender implements IFast
 {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(NettyUDPMessageSender.class);
 	private final SocketAddress remoteAddress;
 	private final DatagramChannel channel;
+	private final ISessionRegistryService sessionRegistryService;
+
 	private static final IDeliveryGuaranty DELIVERY_GUARANTY = DeliveryGuaranty.FAST;
 
 	public NettyUDPMessageSender(SocketAddress remoteAddress,
-			DatagramChannel channel)
+			DatagramChannel channel,
+			ISessionRegistryService sessionRegistryService)
 	{
 		this.remoteAddress = remoteAddress;
 		this.channel = channel;
+		this.sessionRegistryService = sessionRegistryService;
 	}
 
 	@Override
@@ -40,6 +53,21 @@ public class NettyUDPMessageSender implements IFast
 	public IDeliveryGuaranty getDeliveryGuaranty()
 	{
 		return DELIVERY_GUARANTY;
+	}
+
+	@Override
+	public void close()
+	{
+		ISession session = sessionRegistryService.getSession(remoteAddress);
+		if (sessionRegistryService.removeSession(remoteAddress))
+		{
+			LOG.info("Successfully removed session: {}", session);
+		}
+		else
+		{
+			LOG.trace("No udp session found for address: {}", remoteAddress);
+		}
+
 	}
 
 	public SocketAddress getRemoteAddress()
@@ -67,5 +95,10 @@ public class NettyUDPMessageSender implements IFast
 		String sender = "Netty " + channelId + " RemoteAddress: "
 				+ remoteAddress;
 		return sender;
+	}
+
+	public ISessionRegistryService getSessionRegistryService()
+	{
+		return sessionRegistryService;
 	}
 }
