d90ffbc3cbabb738b7057b1d7bad5a55f1da2ec7
menacher <abrahammenacherry@gmail.com>
2012-03-27 19:38:50 +0530

    UnsupportedOperationException may be thrown if setSession is invoked

--- jetserver/src/main/java/org/menacheri/event/ISessionEventHandler.java
+++ jetserver/src/main/java/org/menacheri/event/ISessionEventHandler.java
@@ -6,5 +6,5 @@ public interface ISessionEventHandler extends IEventHandler
 {
 	public ISession getSession();
 
-	public void setSession(ISession session);
+	public void setSession(ISession session) throws UnsupportedOperationException;
 }
