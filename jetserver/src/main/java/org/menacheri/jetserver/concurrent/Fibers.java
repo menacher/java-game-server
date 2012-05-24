3df428d47941263c3772ec1fce04bcb0943e3910
menacher <abrahammenacherry@gmail.com>
2012-05-24 20:45:04 +0530

    Added a map which store the lane against the FiberfactoryPool it is using.

--- jetserver/src/main/java/org/menacheri/jetserver/concurrent/Fibers.java
+++ jetserver/src/main/java/org/menacheri/jetserver/concurrent/Fibers.java
@@ -1,5 +1,6 @@
 package org.menacheri.jetserver.concurrent;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,7 +20,8 @@ public class Fibers
 	// TODO inject this from spring or AppContext
 	private static final ExecutorService SERVICE;
 	private static final PoolFiberFactory FACT;
-
+	private static final ConcurrentHashMap<Lane<String,ExecutorService>, PoolFiberFactory> lanePoolFactoryMap = new ConcurrentHashMap<Lane<String,ExecutorService>, PoolFiberFactory>();
+	
 	static{
 		SERVICE = Executors.newSingleThreadExecutor();
 		FACT = new PoolFiberFactory(SERVICE);
@@ -43,10 +45,27 @@ public class Fibers
 		return fiber;
 	}
 	
+	/**
+	 * Creates and starts a fiber and returns the created instance.
+	 * @return The created fiber.
+	 */
+	public static Fiber pooledFiber(Lane<String,ExecutorService> lane)
+	{
+		if(null == lanePoolFactoryMap.get(lane))
+		{
+			lanePoolFactoryMap.putIfAbsent(lane, new PoolFiberFactory(lane.getUnderlyingLane()));
+		}
+		
+		Fiber fiber = lanePoolFactoryMap.get(lane).create();
+		fiber.start();
+		return fiber;
+	}
+	
 	public static Fiber threadFiber()
 	{
 		Fiber fiber = new ThreadFiber();
 		fiber.start();
 		return fiber;
 	}
+	
 }
