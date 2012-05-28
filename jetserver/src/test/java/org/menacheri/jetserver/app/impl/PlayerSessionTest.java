077235248e5cac237378948adf1e6b3fe5beed4c
menacher <abrahammenacherry@gmail.com>
2012-05-28 12:14:29 +0530

    Added additional assert to check counter

--- jetserver/src/test/java/org/menacheri/jetserver/app/impl/PlayerSessionTest.java
+++ jetserver/src/test/java/org/menacheri/jetserver/app/impl/PlayerSessionTest.java
@@ -1,11 +1,13 @@
 package org.menacheri.jetserver.app.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,17 +15,13 @@ import org.menacheri.jetserver.app.Game;
 import org.menacheri.jetserver.app.GameRoom;
 import org.menacheri.jetserver.app.PlayerSession;
 import org.menacheri.jetserver.app.Session;
-import org.menacheri.jetserver.app.impl.SimpleGame;
-import org.menacheri.jetserver.app.impl.GameRoomSession;
 import org.menacheri.jetserver.app.impl.GameRoomSession.GameRoomSessionBuilder;
-import org.menacheri.jetserver.event.Events;
 import org.menacheri.jetserver.event.Event;
+import org.menacheri.jetserver.event.Events;
 import org.menacheri.jetserver.event.NetworkEvent;
 import org.menacheri.jetserver.event.impl.DefaultSessionEventHandler;
 import org.menacheri.jetserver.protocols.Protocol;
 import org.menacheri.jetserver.protocols.impl.DummyProtocol;
-
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicLong;
 
 public class PlayerSessionTest
 {
@@ -68,7 +66,7 @@ public class PlayerSessionTest
 		}
 	}
 
-	//@Test
+	@Test
 	public void eventHandlingPerformance() throws InterruptedException
 	{
 		long start = System.nanoTime();
@@ -92,6 +90,8 @@ public class PlayerSessionTest
 		t.start();
 
 		assertTrue(LATCH.await(20, TimeUnit.SECONDS));
+		assertEquals(LATCH_COUNT,COUNTER.get());
+		
 		long time = System.nanoTime() - start;
 		System.out.printf(
 				"Took  %.3f seconds to pass %d messages between sessions\n",
