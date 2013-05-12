17194ca4036610c4bb3fcaa627306bb8eb394cc9
aby <abrahammenacherry@gmail.com>
2013-05-12 11:45:31 +0530

    Issue #46 added disconnect method to latchcounter

--- jetserver/src/test/java/org/menacheri/jetserver/util/SessionHandlerLatchCounter.java
+++ jetserver/src/test/java/org/menacheri/jetserver/util/SessionHandlerLatchCounter.java
@@ -4,6 +4,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.menacheri.jetserver.app.Session;
+import org.menacheri.jetserver.event.Event;
 import org.menacheri.jetserver.event.NetworkEvent;
 import org.menacheri.jetserver.event.impl.DefaultSessionEventHandler;
 
@@ -23,8 +24,18 @@ public class SessionHandlerLatchCounter extends DefaultSessionEventHandler {
 	public void onNetworkMessage(NetworkEvent event) {
 		counter.incrementAndGet();
 		latch.countDown();
+		System.out.println("invoked onNetworkMessage");
 	}
 
+	@Override
+	protected void onDisconnect(Event event)
+	{
+		counter.incrementAndGet();
+		latch.countDown();
+		System.out.println("invoked onDisconnect");
+		super.onDisconnect(event);
+	}
+	
 	public AtomicLong getCounter() {
 		return counter;
 	}
