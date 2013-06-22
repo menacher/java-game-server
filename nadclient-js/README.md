e600f96f0f68aa1543b5fe4486ee7b53a7ebe92b
aby <abrahammenacherry@gmail.com>
2013-06-22 01:19:38 -0400

    #50 renaming javascript files and variables to match new project name

--- nadclient-js/README.md
+++ nadclient-js/README.md
@@ -1,15 +1,15 @@
-This is a **Javascript** client project for [jetserver](https://github.com/menacher/java-game-server/tree/master/jetserver) library. An example html which can connect to a locally running Jetserver is located at **test/jetclient.html**.
+This is a **Javascript** client project for [Nadron](https://github.com/menacher/java-game-server/tree/netty4/nadron) library. An example html which can connect to a locally running Nadron is located at **test/nadclient.html**.
 
 About the Client
 ================
-Click on `Start War!` button after loading jetclient.html in a websocket compatible browser to start the game. Assumption is that Jetserver is running and jetclient.html is using accurate hostname and port number.
+Click on `Start War!` button after loading jetclient.html in a websocket compatible browser to start the game. Assumption is that Nadron server is running and nadclient.html is using accurate hostname and port number.
 
 Usage as your own game client
 =============================
 The general usage steps could be as outlined below.    
 1.  Create a `config` object containing the `user`, `password` and `connectionkey` to connect to game room. If you are using a different protocol, then add the appropriate `CodeChain`'s also to the `config` object. By default `JSon` encoding/decoding is used.     
-2.  Create a `session`(s) using the `SessionFactory` by passing in a `url` for the remote jetserver, `config` object and a `callback` function which will receive the `session` object after successful login to remote jeteserver.    
-3.  Add necessary handlers to the session using `addHandler` function. The default events are provided the `jet` class for e.g `jet.lOG_IN`. At the very least you would want to add a handler for the `jet.SESSION_MESSAGE` event to receive incoming events from jetserver.    
+2.  Create a `session`(s) using the `SessionFactory` by passing in a `url` for the remote Nadron server, `config` object and a `callback` function which will receive the `session` object after successful login to remote Nadron server.    
+3.  Add necessary handlers to the session using `addHandler` function. The default events are provided the `nad` class for e.g `nad.lOG_IN`. At the very least you would want to add a handler for the `nad.SESSION_MESSAGE` event to receive incoming events from jetserver.    
 4.  Event objects to be sent to server can be created using the `jet.nevent` function.    
 5.  Data can be send to remote server using `session.send` function.    
     
--- nadclient-js/src/nad-0.1.js
+++ nadclient-js/src/nad-0.1.js
@@ -1,42 +1,42 @@
-(function (jet) {
+(function (nad) {
     "use strict";
 
     // Event code Constants
-    jet.ANY = 0x00;
-    jet.PROTOCOL_VERSION = 0x01;
+    nad.ANY = 0x00;
+    nad.PROTOCOL_VERSION = 0x01;
 
-    jet.CONNECT = 0x02;
-    jet.RECONNECT = 0x03;
-    jet.CONNECT_FAILED = 0x06;
-    jet.LOG_IN = 0x08;
-    jet.LOG_OUT = 0x0a;
-    jet.LOG_IN_SUCCESS = 0x0b;
-    jet.LOG_IN_FAILURE = 0x0c;
-    jet.LOG_OUT_SUCCESS = 0x0e;
-    jet.LOG_OUT_FAILURE = 0x0f;
+    nad.CONNECT = 0x02;
+    nad.RECONNECT = 0x03;
+    nad.CONNECT_FAILED = 0x06;
+    nad.LOG_IN = 0x08;
+    nad.LOG_OUT = 0x0a;
+    nad.LOG_IN_SUCCESS = 0x0b;
+    nad.LOG_IN_FAILURE = 0x0c;
+    nad.LOG_OUT_SUCCESS = 0x0e;
+    nad.LOG_OUT_FAILURE = 0x0f;
 
-    jet.GAME_LIST = 0x10;
-    jet.ROOM_LIST = 0x12;
-    jet.GAME_ROOM_JOIN = 0x14;
-    jet.GAME_ROOM_LEAVE = 0x16;
-    jet.GAME_ROOM_JOIN_SUCCESS = 0x18;
-    jet.GAME_ROOM_JOIN_FAILURE = 0x19;
+    nad.GAME_LIST = 0x10;
+    nad.ROOM_LIST = 0x12;
+    nad.GAME_ROOM_JOIN = 0x14;
+    nad.GAME_ROOM_LEAVE = 0x16;
+    nad.GAME_ROOM_JOIN_SUCCESS = 0x18;
+    nad.GAME_ROOM_JOIN_FAILURE = 0x19;
 
     //Event sent from server to client to start message sending from client to server.
-    jet.START = 0x1a;
+    nad.START = 0x1a;
     // Event sent from server to client to stop messages from being sent to server.
-    jet.STOP = 0x1b;
+    nad.STOP = 0x1b;
     // Incoming data from server or from another session.
-    jet.SESSION_MESSAGE = 0x1c;
+    nad.SESSION_MESSAGE = 0x1c;
     // This event is used to send data from the current machine to remote server
-    jet.NETWORK_MESSAGE = 0x1d;
-    jet.CHANGE_ATTRIBUTE = 0x20;
-    jet.DISCONNECT = 0x22;// Use this one for handling close event of ws.
-    jet.EXCEPTION = 0x24;
+    nad.NETWORK_MESSAGE = 0x1d;
+    nad.CHANGE_ATTRIBUTE = 0x20;
+    nad.DISCONNECT = 0x22;// Use this one for handling close event of ws.
+    nad.EXCEPTION = 0x24;
 
     // Functions
     // Creates a new event object
-    jet.NEvent = function (eventType, payload, session, date) {
+    nad.NEvent = function (eventType, payload, session, date) {
         return {
             type : eventType,
             source : payload,
@@ -45,26 +45,26 @@
         };
     };
 
-    //Special event creation function to send the json class name to jetserver.
-    jet.CNameEvent = function (className) {
+    //Special event creation function to send the json class name to Nadron server.
+    nad.CNameEvent = function (className) {
         return {
-            type : jet.NETWORK_MESSAGE,
+            type : nad.NETWORK_MESSAGE,
             cName : className,
             timeStamp : new Date().getTime()
         };
     };
     
-    // Creates a login event object to login to remote jetserver
-    jet.LoginEvent = function (config) {
-        return jet.NEvent(jet.LOG_IN, [config.user, config.pass, config.connectionKey]);
+    // Creates a login event object to login to remote Nadron server
+    nad.LoginEvent = function (config) {
+        return nad.NEvent(nad.LOG_IN, [config.user, config.pass, config.connectionKey]);
     };
 
     // If using a differnt protocol, then use this codec chain, to decode and encode incoming and outgoing requests. Something like a Chain of Responsibility pattern.
-    jet.CodecChain = function () {
+    nad.CodecChain = function () {
         this.chain = [];
     };
 
-    jet.CodecChain.prototype.add = function (func) {
+    nad.CodecChain.prototype.add = function (func) {
         if (func && typeof (func) === 'function') {
             this.chain.push(func);
         } else {
@@ -73,11 +73,11 @@
         return this;
     };
 
-    jet.CodecChain.prototype.remove = function (func) {
+    nad.CodecChain.prototype.remove = function (func) {
         removeFromArray(this.chain, func);
     };
 
-    jet.CodecChain.prototype.tranform = function (message) {
+    nad.CodecChain.prototype.tranform = function (message) {
         var i = 0;
         for(;i<this.chain.length(); i++){
             message = this.chain[i].transform(message);
@@ -86,12 +86,12 @@
     };
 
     // Default codes which use JSON to decode and encode messages.
-    jet.Codecs = {
+    nad.Codecs = {
         encoder : {transform: function (e){ return JSON.stringify(e)}},
         decoder : {transform: function (e){
                         var evt = JSON.parse(e);
-                        if((typeof evt.type !== 'undefined') && (evt.type === jet.NETWORK_MESSAGE)){
-                            evt.type = jet.SESSION_MESSAGE;
+                        if((typeof evt.type !== 'undefined') && (evt.type === nad.NETWORK_MESSAGE)){
+                            evt.type = nad.SESSION_MESSAGE;
                         }
                         return evt;
                     }
@@ -99,21 +99,21 @@
     };
 
     // Generally a client needs only one session to the server. But this function can be used to create more.
-    jet.sessionFactory = function (url, config, callback) {
+    nad.sessionFactory = function (url, config, callback) {
         new Session(url,config,callback);
     };
 
-    jet.reconnect = function (session, reconnectPolicy, callback) {
+    nad.reconnect = function (session, reconnectPolicy, callback) {
         reconnectPolicy(session, callback);
     };
 
-    // Used to create a session. Once START event is received from the remote jetserver then the callback is invoked with the created session.
+    // Used to create a session. Once START event is received from the remote Nadron server then the callback is invoked with the created session.
     function Session(url, config, callback) {
         var me = this;
         var onStart = callback;
         var callbacks = {};
-        me.inCodecChain = (typeof config.inCodecChain === 'undefined') ? jet.Codecs.decoder : config.inCodecChain;
-        me.outCodecChain = (typeof config.outCodecChain === 'undefined') ? jet.Codecs.encoder : config.outCodecChain;
+        me.inCodecChain = (typeof config.inCodecChain === 'undefined') ? nad.Codecs.decoder : config.inCodecChain;
+        me.outCodecChain = (typeof config.outCodecChain === 'undefined') ? nad.Codecs.encoder : config.outCodecChain;
         var message = getLogin(config);
         var ws = connectWebSocket(url);
         var state = 0;// 0=CONNECTING, 1=CONNECTED, 2=NOT CONNECTED, 3=CLOSED
@@ -127,26 +127,26 @@
                 } else if (state === 2) {
                     ws.send(getReconnect(config));
                 } else {
-                    var evt = jet.NEvent(jet.EXCEPTION,"Cannot reconnect when session state is: " + state);
+                    var evt = nad.NEvent(nad.EXCEPTION,"Cannot reconnect when session state is: " + state);
                     me.onerror(evt);
-                    dispatch(jet.EXCEPTION, evt);
+                    dispatch(nad.EXCEPTION, evt);
                 }
             };
         
-            // Login to jetserver when the start event is received the callback will return the session.
+            // Login to Nadron server when the start event is received the callback will return the session.
             ws.onmessage = function (e) {
-                var loginDecoder = (typeof config.loginDecoder === 'undefined') ? jet.Codecs.decoder : config.loginDecoder;
+                var loginDecoder = (typeof config.loginDecoder === 'undefined') ? nad.Codecs.decoder : config.loginDecoder;
                 var evt = loginDecoder.transform(e.data);
                 if(!evt.type){
                     throw new Error("Event object missing 'type' property.");
                 }
-                if(evt.type === jet.LOG_IN_FAILURE || evt.type === jet.GAME_ROOM_JOIN_FAILURE){
+                if(evt.type === nad.LOG_IN_FAILURE || evt.type === nad.GAME_ROOM_JOIN_FAILURE){
                     ws.close();
                 }
-                if(evt.type === jet.GAME_ROOM_JOIN_SUCCESS) {
+                if(evt.type === nad.GAME_ROOM_JOIN_SUCCESS) {
                     reconnectKey = (typeof evt.source === 'undefined') ? reconnectKey : evt.source;
                 }
-                if(evt.type === jet.START){
+                if(evt.type === nad.START){
                     if (onStart && typeof(onStart) === 'function') {
                         state = 1;
                         applyProtocol(config);
@@ -157,12 +157,12 @@
 
             ws.onclose = function (e) {
                 state = 2;  
-                dispatch(jet.DISCONNECT, jet.NEvent(jet.DISCONNECT, e, me));
+                dispatch(nad.DISCONNECT, nad.NEvent(nad.DISCONNECT, e, me));
             };
 
             ws.onerror = function (e) {
                 state = 2;
-                dispatch(jet.EXCEPTION, jet.NEvent(jet.EXCEPTION, e, me));
+                dispatch(nad.EXCEPTION, nad.NEvent(nad.EXCEPTION, e, me));
             };
             return ws;
         }
@@ -177,7 +177,7 @@
         me.on = me.onevent;// alias for onevent
 
         me.send = function (evt) {
-            if(state !== 1 && !((evt.type === jet.RECONNECT) && (state === 2))){
+            if(state !== 1 && !((evt.type === nad.RECONNECT) && (state === 2))){
                throw new Error("Session is not in connected state"); 
             }
             ws.send( me.outCodecChain.transform(evt) ); // <= send JSON/Binary data to socket server
@@ -204,7 +204,7 @@
         me.close = function () {
             state = 3;
             ws.close();
-            dispatch(jet.CLOSED, jet.NEvent(jet.CLOSED));
+            dispatch(nad.CLOSED, nad.NEvent(nad.CLOSED));
         };
         
         me.disconnect = function () { 
@@ -226,13 +226,13 @@
             if (typeof evt.target === 'undefined') {
                 evt.target = me;
             }
-            if (eventName === jet.SESSION_MESSAGE){
+            if (eventName === nad.SESSION_MESSAGE){
                 me.onmessage(evt);
             }
-            if (eventName === jet.CLOSED){
+            if (eventName === nad.CLOSED){
                 me.onclose(evt);
             }
-            dispatchToEventHandlers(callbacks[jet.ANY], evt);
+            dispatchToEventHandlers(callbacks[nad.ANY], evt);
             dispatchToEventHandlers(callbacks[eventName], evt);
         }
         
@@ -244,14 +244,14 @@
         }
         
         function getLogin(config) {
-            var loginEncoder = (typeof config.loginEncoder === 'undefined') ? jet.Codecs.encoder : config.loginEncoder;
-            return loginEncoder.transform(jet.LoginEvent(config));
+            var loginEncoder = (typeof config.loginEncoder === 'undefined') ? nad.Codecs.encoder : config.loginEncoder;
+            return loginEncoder.transform(nad.LoginEvent(config));
         }
         
         function getReconnect(config) {
             if (typeof reconnectKey === 'undefined') throw new Error("Session does not have reconnect key");
-            var loginEncoder = (typeof config.loginEncoder === 'undefined') ? jet.Codecs.encoder : config.loginEncoder;
-            return loginEncoder.transform(jet.NEvent(jet.RECONNECT, reconnectKey));
+            var loginEncoder = (typeof config.loginEncoder === 'undefined') ? nad.Codecs.encoder : config.loginEncoder;
+            return loginEncoder.transform(nad.NEvent(nad.RECONNECT, reconnectKey));
         }
         
         function applyProtocol(config) {
@@ -266,7 +266,7 @@
         function doNothing(evt) {}
     }
 
-    jet.ReconnetPolicies = {
+    nad.ReconnetPolicies = {
         noReconnect : function (session, callback) { session.close() } ,
         reconnectOnce : function (session, callback) {
             session.reconnect(callback);
@@ -283,4 +283,4 @@
         }
     }
 
-}( window.jet = window.jet || {}));
+}( window.nad = window.nad || {}));
